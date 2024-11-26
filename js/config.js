7aa68a04b71581403c10a6157a96784bb1f21dfd
Акбар Рысбеков <144217684+GoDRA77@users.noreply.github.com>
2024-11-26 14:48:35 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://8is032kxk9.execute-api.us-east-1.amazonaws.com/dev1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://bgcge47at8.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
