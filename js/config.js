7b0945c45fd0ecfa76c95359a8f95ed22fa0895b
Акбар Рысбеков <144217684+GoDRA77@users.noreply.github.com>
2024-11-26 13:09:01 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_sF0sMu7H7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44cv63ucd2h6tu4dmooij8rp2l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
